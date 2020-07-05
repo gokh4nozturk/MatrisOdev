37028e363f590a37df7f5a41029f6c81ecfdf4e1
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-07-05 15:49:21 +0300

    labirent tamamlandı.

--- MatrisOdev/Program.cs
+++ MatrisOdev/Program.cs
@@ -8,6 +8,7 @@ namespace MatrisOdev
         //Değişken isimlendirme formatı olarak Camel Case kullanılmıştır.
 
         public static Random rnd = new Random();
+        public static Random rnd2 = new Random();
         public static int[,] matris = new int[10, 10];
         public static string yon = "";
 
@@ -22,52 +23,105 @@ namespace MatrisOdev
         public static int baslangicY = 0;
 
         //labirentin içindeki yollar.
-        public static int yolX = 1;
-        public static int yol1Y = rnd.Next(1, 9);
+        public static int yolX = 0;
+        public static int yol1Y = rnd.Next(0, 3);
         public static int isControlYol1Y = yol1Y;
-        public static int yol2Y = rnd.Next(1, 9);
+        public static int yol2X = 0;
+        public static int yol2Y= rnd.Next(3, 5);
+        public static int yol3X = 0;
+        public static int yol3Y = rnd.Next(5, 9);
 
+        //yolun genisligini ayarlamak icin kullanılıyor.
+        public static int genislik = 0;
+
+        //matrisin saf halini çizer.
         public static void MatrisCiz()
         {
             //yollar için çakışmama durumunu kontrol etmemiz gereken yer burasıdır.
-            for (int i = 0; i < 10; i++)
+            if (yol1Y == yol2Y || yol1Y == yol3Y) yol1Y = rnd2.Next(0, 3);
+            else if (yol2Y == yol1Y || yol2Y == yol3Y) yol2Y = rnd2.Next(3, 5);
+            else if (yol3Y == yol2Y || yol3Y == yol1Y) yol3Y = rnd2.Next(5, 9);
+            else
             {
-                for (int j = 0; j < 10; j++)
+                for (int i = 0; i < 10; i++)
                 {
-                    if (i == yolX && j == yol1Y)
+                    for (int j = 0; j < 10; j++)
                     {
-                        matris[i, j] = 1;//yol dışına çıkıp çıkmama durumunu dizinin değerinden kontrol edeceğiz.
-                        yolX++;//yol için her satırda alta inmesini sağlıyoruz.
-                        if ((yol1Y >= 1 && yol1Y < 8) && matris[i, j] == 1)
+                        if (i == yolX && j == yol1Y)
                         {
-                            yol1Y++;
+                            matris[i, j] = 1;//yol dışına çıkıp çıkmama durumunu dizinin değerinden kontrol edeceğiz.
+                            yolX++;//yol için her satırda alta inmesini sağlıyoruz.
+                            if ((yol1Y >= 1 && yol1Y < 4) && matris[i, j] == 1)
+                            {
+                                yol1Y++;
+                            }
+                            else if (yol1Y > 1 && matris[i, j] == 1)
+                            {
+                                yol1Y -= 1;
+                            }
                         }
-                        else if (yol1Y > 1 && matris[i, j] == 1)
+                        else if (i == yol2X && j == yol2Y)
                         {
-                            yol1Y -= 1;
+                            matris[i, j] = 1;//yol dışına çıkıp çıkmama durumunu dizinin değerinden kontrol edeceğiz.
+                            yol2X++;//yol için her satırda alta inmesini sağlıyoruz.
+                            if ((yol2Y >= 3 && yol2Y < 6) && matris[i, j] == 1)
+                            {
+                                yol2Y++;
+                            }
+                            else if (yol2Y > 1 && matris[i, j] == 1)
+                            {
+                                yol2Y -= 1;
+                            }
                         }
-                    }
-                    else if (i == yolX && j == yol2Y)
-                    {
-                        matris[i, j] = 2;//yol dışına çıkıp çıkmama durumunu dizinin değerinden kontrol edeceğiz.
-                        yolX++;//yol için her satırda alta inmesini sağlıyoruz.
-                        if ((yol2Y >= 1 && yol2Y < 8) && matris[i, j] == 2)
+                        else if (i == yol3X && j == yol3Y)
                         {
-                            yol2Y++;
+                            matris[i, j] = 1;//yol dışına çıkıp çıkmama durumunu dizinin değerinden kontrol edeceğiz.
+                            yol3X++;//yol için her satırda alta inmesini sağlıyoruz.
+                            if ((yol3Y >= 5 && yol3Y < 9) && matris[i, j] == 1)
+                            {
+                                yol3Y++;
+                            }
+                            else if (yol3Y > 1 && matris[i, j] == 1)
+                            {
+                                yol3Y -= 1;
+                            }
+                            else if(yol3Y == yol2Y ||yol3Y == yol1Y)
+                            {
+                                yol3Y -= 3;
+                            }
                         }
-                        else if (yol2Y > 1 && matris[i, j] == 2)
+                        else
                         {
-                            yol2Y -= 1;
+                            matris[i, j] = 0;
                         }
-                    }
-                    else
-                    {
-                        matris[i, j] = 0;
                     }
                 }
             }
         }
 
+        //matris çizildikten sonra aralarda kalan boşlukları doldurur.
+        public static void MatrisDuzenle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (matris[i, j] == 1 && 8 >= j && i <= 8 && genislik < 3)
+                    {
+                        //Console.WriteLine("{0}.satır {1}. sütun : {2}",i, j, matris[i, j + 1]);
+                        genislik++;
+                        matris[i, j + 1] = 1;
+                    }
+                    else if (matris[i, j] == 1 && i == 9)
+                    {
+                        matris[i - 1, j] = 1;
+                    }
+                    else genislik = 0;
+                }
+            }
+        }
+
+        //matrisin son halini ekrana basar.
         public static void MatrisGetir()
         {
             for (int i = 0; i < 10; i++)
@@ -76,17 +130,10 @@ namespace MatrisOdev
                 {
                     if (matris[i,j] == 1)
                     {
-                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.Write(matris[i, j]);
-                        Console.ResetColor();
                         Console.Write("|");
-                    }
-                    else if (matris[i, j] == 2)
-                    {
-                        Console.BackgroundColor = ConsoleColor.DarkGreen;
-                        Console.Write(matris[i, j]);
                         Console.ResetColor();
-                        Console.Write("|");
                     }
                     else
                     {
@@ -103,11 +150,15 @@ namespace MatrisOdev
             Console.WriteLine("Oynama şekli: yeşil renkli alan başlangıç noktasını gösterir.\nw: yukarı, a: sola, s: aşağı, d: sağa yönlendirir.\n");
 
             MatrisCiz();
+            MatrisDuzenle();
             MatrisGetir();
 
             //Console.Write("Yön belirtiniz :");
             //yon = Console.ReadLine();
             //Console.WriteLine(yon);
+
+            
+
             Console.ReadKey();
         }
     }
